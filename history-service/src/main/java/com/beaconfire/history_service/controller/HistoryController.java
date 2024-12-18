1963c0b2b079773e7a9a81f1c06e8e50e11cf5b0
Ricky <ruiqizhang8279@outlook.com>
2024-12-18 15:25:37 -0800

    History Config

--- history-service/src/main/java/com/beaconfire/history_service/controller/HistoryController.java
+++ history-service/src/main/java/com/beaconfire/history_service/controller/HistoryController.java
@@ -5,15 +5,21 @@ import com.beaconfire.history_service.dto.HistoryResponseDTO;
 import com.beaconfire.history_service.service.HistoryService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping("/api/history")
 @Slf4j
 public class HistoryController {
     private final HistoryService historyService;
+
+    @Value("${user.role}")
+    private String userRole;
 
     public HistoryController(HistoryService historyService) {
         this.historyService = historyService;
@@ -24,6 +30,12 @@ public class HistoryController {
             @PathVariable Integer userId) {
         log.info("Received request to get history for user {}", userId);
         return ResponseEntity.ok(historyService.getUserHistory(userId));
+    }
+
+    @GetMapping("/config")
+    public ResponseEntity<String> getConfig() {
+        log.info("Received request to get configuration");
+        return ResponseEntity.ok(userRole);
     }
 
     @PostMapping
