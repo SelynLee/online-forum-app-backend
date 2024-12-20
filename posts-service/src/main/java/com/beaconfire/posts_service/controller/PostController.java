6a963ad8adb57703c7e5420c555437b7b1fc4257
Ricky <ruiqizhang8279@outlook.com>
2024-12-20 00:32:38 -0800

    add history-service and message service to api-gateway, add message-service to eureka

--- posts-service/src/main/java/com/beaconfire/posts_service/controller/PostController.java
+++ posts-service/src/main/java/com/beaconfire/posts_service/controller/PostController.java
@@ -5,16 +5,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.beaconfire.posts_service.domain.Accessibility;
 import com.beaconfire.posts_service.domain.Metadata;
@@ -36,7 +27,6 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/posts")
 @Tag(name = "Post Service", description = "Endpoints for managing posts")
-
 public class PostController {
 
    
